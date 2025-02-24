5131d3785b6dc82f6253eafb4e24b3f5b092dd9f
fermon99 <filipraeder01@gmail.com>
2025-02-24 13:37:26 +0100

    Finished classes Die and Dice (no tests run yet) Commited 24/02/2025

--- src/main/java/com/gruppe25/Dice.java
+++ src/main/java/com/gruppe25/Dice.java
@@ -1,22 +1,28 @@
 package com.gruppe25;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Dice {
-  private List<Die> dice;
+  private final List<Die> dice;
 
   public Dice(int numberOfDice) {
+    dice = new ArrayList<>();
     for (int i = 0; i < numberOfDice; i++) {
-        dice.add(e);
+      dice.add(new Die());
     }
   }
 
   public int roll() {
-    
+    int result = 0;
+    for (Die die : dice) {
+      result += die.roll();
+    }
+    return result;
   }
 
   public int getDie(int dieNumber) {
-
+    return dice.get(dieNumber+1).getValue();
   }
 
 }
