339bc9390961aa9545dfd30f731e3364a2c69387
Filip Ræder <filipraeder01@gmail.com>
2025-03-27 08:54:16 +0100

    Small changes committed 27/03/2025

--- src/main/java/com/gruppe25/BoardGameApp.java
+++ src/main/java/com/gruppe25/BoardGameApp.java
@@ -1,15 +1,15 @@
 package com.gruppe25;
 
-import static com.gruppe25.PlayerReader.readPlayersFromCSV;
-
 import java.util.List;
+
+import static com.gruppe25.PlayerReader.readPlayersFromCSV;
 
 public class BoardGameApp {
   public BoardGameApp() {
 
   }
 
-  public void startGame(String playerFileName){
+  public void startGame(String playerFileName /*, String boardFileName */){
     BoardGame boardgame = new BoardGame();
 
     /*boardgame.addPlayer(new Player("Filip"));
@@ -17,6 +17,7 @@ public class BoardGameApp {
     boardgame.addPlayer(new Player("Bot 2"));
     boardgame.addPlayer(new Player("Bot 3"));*/
 
+    /* Adding all players from file */
     List<Player> players = readPlayersFromCSV(playerFileName);
     for (Player player : players) {
       System.out.println(player);
--- src/main/java/com/gruppe25/PlayerReader.java
+++ src/main/java/com/gruppe25/PlayerReader.java
@@ -1,12 +1,12 @@
 package com.gruppe25;
 
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvValidationException;
-
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 
 public class PlayerReader {
   public static List<Player> readPlayersFromCSV(String filename) {
@@ -20,7 +20,6 @@ public class PlayerReader {
           players.add(new Player(name));
         }
     } catch (IOException | NumberFormatException | CsvValidationException e) {
-      e.printStackTrace();
     } 
     return players;
   }
